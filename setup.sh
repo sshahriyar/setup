52c3eff9c2df677e78294ca17c560609d069a295
sshahriyar <syed.shariyar@gmail.com>
2013-07-17 00:13:22 -0400

    Updated emacs backsapace configuration

--- setup.sh
+++ setup.sh
@@ -41,4 +41,6 @@ ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
 
+echo "(define-key global-map \"\C-h\" 'backward-delete-char)" > .emacs
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
